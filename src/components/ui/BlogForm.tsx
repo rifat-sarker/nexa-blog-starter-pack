d479a7b559ae92cb1aa46565b6b5dd4f6e1cb7b6
rifat-sarker <rifatswd@gmail.com>
2025-02-13 21:33:41 +0600

    Creating Blogs Using Next.js Server Actions

--- src/components/ui/BlogForm.tsx
+++ src/components/ui/BlogForm.tsx
@@ -1,3 +1,4 @@
+import { createBlog } from '@/actions/createBlog';
 import Form from 'next/form'
 
 
@@ -6,7 +7,7 @@ const BlogForm = () => {
   return (
     <div className="flex items-center justify-center min-h-screen my-10">
       <div className="w-full max-w-4xl bg-white shadow-lg rounded-lg p-6">
-        <Form action="/blogs">
+        <Form action={createBlog}>
           <h2 className="text-2xl font-semibold mb-6 text-teal-600 text-center">
             Create Blog
           </h2>
