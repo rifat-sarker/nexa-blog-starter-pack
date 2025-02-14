064e076eb71683992682ae29d2be4e3c22103a91
rifat-sarker <rifatswd@gmail.com>
2025-02-14 11:45:59 +0600

    Technical Breakdown of Client-Side Data Fetching (RTK Query) and Displaying Blogs and Handling Loading with RTK Query

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,20 +1,37 @@
+"use client";
+
 import BlogCard from "@/components/ui/BlogCard";
+import Spinner from "@/components/ui/Spinner";
+import { useGetBlogsQuery } from "@/redux/apis/blog.slice";
 import { Blog } from "@/types";
-import { Metadata } from "next";
+// import { Metadata } from "next";
 
-export const metadata:Metadata = {
-  title: "NexaBlog | Blogs"
-}
+// export const metadata:Metadata = {
+//   title: "NexaBlog | Blogs"
+// }
 
-const BlogPage = async() => {
-    const res = await fetch('http://localhost:5000/blogs', {
-    cache: "no-store"
-    })
+const BlogPage = () => {
+  // const res = await fetch('http://localhost:5000/blogs', {
+  // cache: "no-store"
+  // })
 
-    const blogs = await res.json();
-    return (
-     <div className="w-[90%] mx-auto">
-            <h1 className="text-3xl text-center my-5 font-bold">
+  const { data: blogs, isLoading, isError } = useGetBlogsQuery({});
+
+  // console.log(data, isError);
+  // const blogs = await res.json();
+  if(isLoading){
+    return <Spinner/>
+  }
+
+
+   // Handle errors (optional)
+   if (isError) {
+    return <div>Error loading blogs...</div>;
+  }
+
+  return (
+    <div className="w-[90%] mx-auto">
+      <h1 className="text-3xl text-center my-5 font-bold">
         Explore all <span className="text-teal-600">Blogs</span>
       </h1>
       <p className="text-center text-gray-400 w-2/5 mx-auto">
@@ -24,11 +41,12 @@ const BlogPage = async() => {
         </i>
       </p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {blogs.map((blog: Blog) =>
-          <BlogCard key={blog.id} blog={blog} /> )}
+        {blogs.map((blog: Blog) => (
+          <BlogCard key={blog.id} blog={blog} />
+        ))}
       </div>
-      </div>
-    );
+    </div>
+  );
 };
 
 export default BlogPage;
