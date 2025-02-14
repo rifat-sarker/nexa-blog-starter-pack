3a1fe4bf776b363633a13e5d51b683f17f89e62b
rifat-sarker <rifatswd@gmail.com>
2025-02-14 08:46:01 +0600

    Generating Dynamic Metadata with generateMetadata

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,4 +1,28 @@
 import BlogDetailsCard from "@/components/ui/BlogDetailsCard";
+import { Blog } from "@/types";
+
+export const generateStaticParams = async () => {
+  const res = await fetch("http://localhost:5000/blogs");
+  const blogs = await res.json();
+  return blogs.slice(0, 3).map((blog: Blog) => ({
+    blogId: blog.id,
+  }));
+};
+
+// or Dynamic metadata
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ blogId: string }>;
+}) {
+  const { blogId } = await params;
+  const res = await fetch(`http://localhost:5000/blogs/${blogId}`);
+  const blog = await res.json();
+
+  return {
+    title: blog.title,
+  };
+}
 
 export default async function BlogDetailsPage({
   params,
@@ -9,5 +33,9 @@ export default async function BlogDetailsPage({
   const res = await fetch(`http://localhost:5000/blogs/${blogId}`);
   const blog = await res.json();
 
-  return <BlogDetailsCard blog={blog} />;
+  return (
+    <div>
+      <BlogDetailsCard blog={blog} />
+    </div>
+  );
 }
